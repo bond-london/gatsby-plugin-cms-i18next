8027ada5a775e23e32d3f9114f8e22110ab25446
nrandell <nick@nickrandell.com>
2021-11-05 19:33:07 +0000

    fix: use window __replace

--- src/LanguageDetect.tsx
+++ src/LanguageDetect.tsx
@@ -4,6 +4,9 @@ import React from 'react';
 import {removePathPrefix, useI18next} from '.';
 import {LANGUAGE_KEY} from './types';
 
+interface GatsbyWindow {
+  __replace: (to: string) => void;
+}
 export const LanguageDetect: React.FC = () => {
   const {defaultLanguage, languages, hasTranslations} = useI18next();
   if (hasTranslations) {
@@ -20,7 +23,8 @@ export const LanguageDetect: React.FC = () => {
     const {search, pathname, hash} = location;
     const queryParams = search || '';
     const newUrl = withPrefix(`/${desired}${removePathPrefix(pathname)}${queryParams}${hash}`);
-    location.replace(newUrl);
+    const gatsbyWindow = window as unknown as GatsbyWindow;
+    gatsbyWindow.__replace(newUrl);
     return null;
   }
   return <h1>Language should switch on the browser</h1>;
